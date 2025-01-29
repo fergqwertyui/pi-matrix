d27ce9acbfaa51c0c05f32909b7f041f4a51ab28
Fergus Robson <fergusrobson2@gmail.com>
2025-01-29 21:16:25 +0000

    Added venv to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 echo "Ensure packages are installed:"
-sudo apt-get install libopenjp2-7 python3-dbus
+sudo apt-get install -y libopenjp2-7 python3-dbus python3-venv
 
 echo "Blacklist soundcard..."
 sudo touch /etc/modprobe.d/alsa-blacklist.conf
-sudo echo "blacklist snd_bcm2835" >> /etc/modprobe.d/alsa-blacklist.conf
+echo "blacklist snd_bcm2835" | sudo tee -a /etc/modprobe.d/alsa-blacklist.conf
+
+install_path=$(pwd)
+venv_path="${install_path}/venv"
+
+echo "Creating a Python virtual environment at ${venv_path}..."
+python3 -m venv $venv_path
+
+echo "Activating virtual environment..."
+source $venv_path/bin/activate
+
+echo "Installing Python dependencies inside virtual environment:"
 
 echo "Installing spotipy library:"
 pip install spotipy==2.23.0
@@ -18,23 +29,31 @@ pip install flask==3.0.0
 
 echo "Enter your Spotify Client ID:"
 read spotify_client_id
+export SPOTIPY_CLIENT_ID=$spotify_client_id
 
 echo "Enter your Spotify Client Secret:"
 read spotify_client_secret
+export SPOTIPY_CLIENT_SECRET=$spotify_client_secret
 
 echo "Enter your Spotify Redirect URI:"
 read spotify_redirect_uri
+export SPOTIPY_REDIRECT_URI=$spotify_redirect_uri
 
-echo "Enter your spotify username:"
+echo "Enter your Spotify username:"
 read spotify_username
 
-echo "Enter the full path to your spotify token:"
+echo "Generating Spotify token..."
+${venv_path}/bin/python python/generateToken.py $spotify_username
+
+echo
+echo "###### Spotify Token Created ######"
+echo "Filename: .cache"
+
+echo "Enter the full path to your Spotify token:"
 read spotify_token_path
 
-install_path=$(pwd)
-
 echo "Downloading rgb-matrix software setup:"
-curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/rgb-matrix.sh >rgb-matrix.sh
+curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/rgb-matrix.sh > rgb-matrix.sh
 
 sed -n '/REBOOT NOW?/q;p' < rgb-matrix.sh > rgb-matrix-spotipi.sh
 
@@ -59,14 +78,15 @@ echo "...done"
 
 echo "Creating spotipi service:"
 sudo cp ./config/spotipi.service /etc/systemd/system/
-sudo sed -i -e "/\[Service\]/a ExecStart=python ${install_path}/python/displayCoverArt.py ${spotify_username} ${spotify_token_path} < /dev/zero &> /dev/null &" /etc/systemd/system/spotipi.service
-sudo mkdir /etc/systemd/system/spotipi.service.d
+sudo sed -i -e "/\[Service\]/a ExecStart=${venv_path}/bin/python ${install_path}/python/displayCoverArt.py ${spotify_username} ${spotify_token_path} < /dev/zero &> /dev/null &" /etc/systemd/system/spotipi.service
+sudo mkdir -p /etc/systemd/system/spotipi.service.d
 spotipi_env_path=/etc/systemd/system/spotipi.service.d/spotipi_env.conf
 sudo touch $spotipi_env_path
-sudo echo "[Service]" >> $spotipi_env_path
-sudo echo "Environment=\"SPOTIPY_CLIENT_ID=${spotify_client_id}\"" >> $spotipi_env_path
-sudo echo "Environment=\"SPOTIPY_CLIENT_SECRET=${spotify_client_secret}\"" >> $spotipi_env_path
-sudo echo "Environment=\"SPOTIPY_REDIRECT_URI=${spotify_redirect_uri}\"" >> $spotipi_env_path
+echo "[Service]" | sudo tee $spotipi_env_path
+echo "Environment=\"SPOTIPY_CLIENT_ID=${spotify_client_id}\"" | sudo tee -a $spotipi_env_path
+echo "Environment=\"SPOTIPY_CLIENT_SECRET=${spotify_client_secret}\"" | sudo tee -a $spotipi_env_path
+echo "Environment=\"SPOTIPY_REDIRECT_URI=${spotify_redirect_uri}\"" | sudo tee -a $spotipi_env_path
+echo "Environment=\"PATH=${venv_path}/bin:\$PATH\"" | sudo tee -a $spotipi_env_path
 sudo systemctl daemon-reload
 sudo systemctl start spotipi
 sudo systemctl enable spotipi
@@ -74,7 +94,7 @@ echo "...done"
 
 echo "Creating spotipi-client service:"
 sudo cp ./config/spotipi-client.service /etc/systemd/system/
-sudo sed -i -e "/\[Service\]/a ExecStart=python ${install_path}/python/client/app.py &" /etc/systemd/system/spotipi-client.service
+sudo sed -i -e "/\[Service\]/a ExecStart=${venv_path}/bin/python ${install_path}/python/client/app.py &" /etc/systemd/system/spotipi-client.service
 sudo systemctl daemon-reload
 sudo systemctl start spotipi-client
 sudo systemctl enable spotipi-client
@@ -84,9 +104,11 @@ echo -n "In order to finish setup a reboot is necessary..."
 echo -n "REBOOT NOW? [y/N] "
 read
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
-        echo "Exiting without reboot."
-        exit 0
+    echo "Exiting without reboot."
+    deactivate
+    exit 0
 fi
+
 echo "Reboot started..."
 reboot
 sleep infinity
